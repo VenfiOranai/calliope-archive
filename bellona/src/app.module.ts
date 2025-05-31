8c22ec19d4057793a9a3bb04b8732771f0f664f3
Venfi <venfioranai@gmail.com>
2025-05-31 20:05:47 +0300

    Added empty controllers for book and user

--- bellona/src/app.module.ts
+++ bellona/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IndexController } from 'src/controllers/index.controller';
+import { UserController } from 'src/controllers/user.controller';
+import { BookController } from 'src/controllers/book.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, IndexController],
+  controllers: [AppController, IndexController, UserController, BookController],
   providers: [AppService],
 })
 export class AppModule {}
