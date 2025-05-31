916392465e650b401feddddf696cc903a5a478a9
Venfi <venfioranai@gmail.com>
2025-05-31 19:59:21 +0300

    Added index route

--- bellona/src/app.module.ts
+++ bellona/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IndexController } from 'src/controllers/index.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, IndexController],
   providers: [AppService],
 })
 export class AppModule {}
