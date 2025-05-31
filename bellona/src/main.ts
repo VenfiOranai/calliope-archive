aa1440360faaad0306c16d3f1eaed8879e9280fe
Venfi <venfioranai@gmail.com>
2025-05-31 19:53:25 +0300

    Enable api versioning

--- bellona/src/main.ts
+++ bellona/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { repos } from 'src/database/repos/repos';
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('Factory');
 
   logger.debug('Initializing...');
   const app = await NestFactory.create(AppModule);
+
+  logger.debug('Enabling URI Versioning');
+  app.enableVersioning({ type: VersioningType.URI });
 
   logger.debug('Enabling cors');
   app.enableCors();
