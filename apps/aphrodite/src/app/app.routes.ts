5e1a6b3045af480f818344db09e9e4e3406f2945
Venfi <venfioranai@gmail.com>
2025-06-07 18:06:51 +0300

    Added empty login page

--- apps/aphrodite/src/app/app.routes.ts
+++ apps/aphrodite/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from '@components/pages/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPage },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+];
